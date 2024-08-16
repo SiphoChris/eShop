cbeadf313f69faa063c9eeeaa48b7b739c143e66
SiphoChris <siphonkebe@gmail.com>
2024-08-16 14:42:05 +0200

    initial setup of the project

--- frontend/shop/src/components/NavComp.vue
+++ frontend/shop/src/components/NavComp.vue
@@ -1,25 +1,45 @@
 <template>
   <nav>
+    <img src="../assets/logo.png" alt="logo" />
     <ul>
-        <li><img src="" alt="logo"></li>
-        <li><a href="">Products</a></li>
-        <li>Users</li>
-        <li>More</li>
-        
+      <li><a href="">Products</a></li>
+      <li><a href="">Users</a></li>
+      <li><a href="">More</a></li>
     </ul>
   </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
+<style src="../../public/style.css">
+</style>
 <style scoped>
-    nav{
-        display: flex;
-        height: 1.3rem;
-        background-color: #2951c9;
-    }
+
+nav, ul{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
+}
+nav {
+  height: 6rem;
+  padding: 1rem 3rem;
+  background-color: #2951c9;
+}
+ul {
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+}
+
+li>a{
+    color: aliceblue;
+    font-size: 1.35rem;
+    text-decoration: none;
+}
+img{
+    height: 3.3rem;
+}
 </style>
